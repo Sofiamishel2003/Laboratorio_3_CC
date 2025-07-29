64b0a6d57ee95870f7e786cf8f9ce900208f7b3b
Sofiamishel2003 <sofiamishel2003@gmail.com>
2025-07-29 09:18:41 -0600

    feat: functional Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,10 +15,10 @@ provider "digitalocean" {
 # We'll be using the smallest vm power there is, just for learning.
 # Not following instructions will result in your grade being negativelly impacted!
 resource "digitalocean_droplet" "web" {
-  image  = "ubuntu-24-10-x64"
+  image  = "ubuntu-20-04-x64"
   name   = "example-droplet"
   region = "nyc1"
-  size   = "s-1vcpu-512mb-10gb"
+  size   = "s-1vcpu-1gb"
 }
 
 output "droplet_ip" {
